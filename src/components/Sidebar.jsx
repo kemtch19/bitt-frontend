2cb4a5a0bb9d8804bc0f513fed0caf7079c47641
kemtch <kemtch19@gmail.com>
2025-10-24 20:39:42 -0500

    feat: update Sidebar component, add logo, and enhance Feed input; integrate DaisyUI for improved styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,29 +5,28 @@ import {
   Smile,
   UserRoundPenIcon,
 } from "lucide-react";
-import logo1 from "../assets/logo1.png";
+import logo1 from "../assets/logo-bitt.svg";
 import { NavLink } from "react-router-dom";
 
 export const Sidebar = () => {
   const rutas = [
     { to: "/", label: "Inicio", icon: HomeIcon },
-    { to: "/saludo", label: "saludame", icon: Smile },
-    { to: "/messages", label: "mensaje", icon: MessageCircleIcon },
-    { to: "/notifications", label: "notificaciones", icon: BellIcon },
-    { to: "/profile", label: "perfil", icon: UserRoundPenIcon },
+    { to: "/saludo", label: "Saludame", icon: Smile },
+    { to: "/messages", label: "Mensaje", icon: MessageCircleIcon },
+    { to: "/notifications", label: "Notificaciones", icon: BellIcon },
+    { to: "/profile", label: "Perfil", icon: UserRoundPenIcon },
   ];
 
   return (
     <div className="flex flex-col p-3">
-      <img className="size-20 " src={logo1} alt="" />
-      <h1>Sidebar</h1>
+      <img className="" src={logo1}  />
       {rutas.map((r) => (
         <NavLink
           className={({ isActive }) =>
-            `flex ${
+            `flex px-2 gap-2 ${
               isActive
-                ? "bg-blue-400 m-2 p-2 rounded-sm"
-                : "hover:bg-gray-600 m-2 p-2 rounded-sm"
+                ? "bg-[#2343CC] text-white m-2 p-2 rounded-sm"
+                : "hover:bg-[#bfc1ca5d] m-2 p-2 rounded-sm"
             } `
           }
           key={r.label}
--- src/page/Feed.jsx
+++ src/page/Feed.jsx
@@ -1,28 +1,44 @@
-import { ClipboardIcon, ClipboardListIcon, ImageIcon, ImagePlus, ImagePlusIcon, SmileIcon, SmilePlusIcon } from "lucide-react";
+import { ClipboardListIcon, ImagePlusIcon, SmilePlusIcon } from "lucide-react";
+import { useRef, useState } from "react";
 
 export const Feed = () => {
-  // 🧠 Logic
+  const [value, setValue] = useState("");
+  const textareaRef = useRef(null);
+
+  const handleInput = (e) => {
+    const textarea = textareaRef.current;
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + "px";
+    setValue(e.target.value);
+  };
 
   return (
     <div className="bg-[#efeff0] h-screen p-3">
       <h1>Inicio</h1>
-      <div className="p-3 flex gap-2 bg-white">
-        <img src="/vite.svg" alt="" />
+      <div className="p-3 flex gap-3 bg-white">
+        <img className="h-fit" src="/vite.svg" alt="" />
         <div>
-          <div className="flex">
-            <input
-              className="border"
-              type="text"
-              placeholder="¿Qué está pasando?"
-            />
-          </div>
+          <textarea
+            ref={textareaRef}
+            value={value}
+            className="input input-ghost w-full rezize-none overflow-hidden bg-white text-black"
+            type="text"
+            placeholder="¿Qué está pasando?"
+            maxLength={100}
+            onInput={handleInput}
+          />
           <div className="flex justify-between">
-            <div className="flex">
-              <ImagePlusIcon color="#EB33BD" />
-              <ClipboardListIcon />
-              <SmilePlusIcon />
+            <div className="flex mt-3 gap-3">
+              <ImagePlusIcon color="#2343CC" />
+              <ClipboardListIcon color="#2343CC" />
+              <SmilePlusIcon color="#2343CC" />
             </div>
-            <button type="submit">Bitt</button>
+            <button
+              className="bg-[#2343CC] text-white mt-2 rounded-lg p-1 pr-3 pl-3 cursor-pointer"
+              type="submit"
+            >
+              Bitt
+            </button>
           </div>
         </div>
       </div>
